e8087667b7fdebb63861a2ba19deafca14c39c77
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 05:34:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
 #write your code here
 
 def countdown (n)
-  n = 11
-  while n > 1
-   n -= 1 
+  n = 10
+  while n > 0 
+   
     puts "#{n} SECOND(S)!"
     
-   break if n ==  12
-    
+    break if n ==  
+    n -= 1 
    
 end
- 
+end 
 puts "HAPPY NEW YEAR!"
 end
 
