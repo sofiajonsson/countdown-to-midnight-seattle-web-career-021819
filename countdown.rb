bc306149cb670dc06e33e7c8e01f677e7d1dd63b
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 04:46:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,14 @@ def countdown (n)
   n = 10
   while n > 0 
     puts "#{n} SECOND(S)!"
+    break if n == 0 || n >10
     n = n - 1
   end
   
 puts "HAPPY NEW YEAR!"
 
 end
-
+end 
 
 # def countdown_with_sleep (n)
 #   sleep(num_secs)
